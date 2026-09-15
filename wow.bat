bd3bc780b2ec0a5228e2e2b70ec14e256850b22e
realh5n <121570327+hsn-lab@users.noreply.github.com>
2026-09-15 12:40:20 +0100

    v1, quite basic

---
+++ wow.bat
@@ -0,0 +1,65 @@
+@ECHO OFF
+echo %DATE%
+CLS
+DIR
+
+DRIVERQUERY
+
+echo %DATE%
+IPCONFIG/ALL
+ASSOC | find ".txt"
+CHKDSK
+
+:: This batch file details Windows 10, hardware, and networking configuration.
+
+TITLE My System Info
+
+ECHO Please wait... Checking system information.
+
+:: Section 1: Windows 10 information
+
+ECHO ==========================
+
+ECHO WINDOWS INFO
+
+ECHO ============================
+
+systeminfo | findstr /c:"OS Name"
+
+systeminfo | findstr /c:"OS Version"
+
+systeminfo | findstr /c:"System Type"
+
+:: Section 2: Hardware information.
+
+ECHO ============================
+
+ECHO HARDWARE INFO
+
+ECHO ============================
+
+systeminfo | findstr /c:"Total Physical Memory"
+
+wmic cpu get name
+
+wmic diskdrive get name,model,size
+
+wmic path win32_videocontroller get name
+
+wmic path win32_VideoController get CurrentHorizontalResolution,CurrentVerticalResolution
+
+:: Section 3: Networking information.
+
+ECHO ============================
+
+ECHO NETWORK INFO
+
+ECHO ============================
+
+ipconfig | findstr IPv4ipconfig | findstr IPv6
+
+cd/Program Files(x86)/Securus/securusclient/
+
+START https://github.com/
+
+PAUSE
